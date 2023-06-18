24850f99c4c4a948bceb3061d3e238ea505e2ca6
linterz <linzhi@msu.edu>
2023-06-17 22:33:01 -0400

    some ublock rules

--- homeworkify/homeworkify.ts
+++ homeworkify/homeworkify.ts
@@ -7,102 +7,102 @@
 // ==/UserScript==
 
 const captchaSubmit = (capInsert) => {
-    let captchaButton = document.getElementById(
-        "verify-captcha"
-    ) as HTMLButtonElement;
+  let captchaButton = document.getElementById(
+    "verify-captcha"
+  ) as HTMLButtonElement;
 
-    capInsert.addEventListener("keydown", (e) => {
-        let keyboardEvent = <KeyboardEvent>e;
-        if (keyboardEvent.key === "Enter") {
-            console.log("Enter pressed for captcha");
-            captchaButton.click();
-            setTimeout(() => {
-                console.log("pressed view solution");
-                let viewSolution = document.getElementById(
-                    "view-solution"
-                ) as HTMLButtonElement;
-                viewSolution?.click();
-            }, 500);
-        }
-    });
+  capInsert.addEventListener("keydown", (e) => {
+    let keyboardEvent = <KeyboardEvent>e;
+    if (keyboardEvent.key === "Enter") {
+      console.log("Enter pressed for captcha");
+      captchaButton.click();
+      setTimeout(() => {
+        console.log("pressed view solution");
+        let viewSolution = document.getElementById(
+          "view-solution"
+        ) as HTMLButtonElement;
+        viewSolution?.click();
+      }, 500);
+    }
+  });
 };
 
 const captcha = () => {
-    console.log("in captcha");
+  console.log("in captcha");
 
-    const observer = new MutationObserver(() => {
-        let notRobotDiv = document.querySelector(
-            ".message.success.loggedin"
-        ) as HTMLElement;
-        if (notRobotDiv !== null) {
-            let notRobotButton = document.querySelector(
-                ".captcha_container"
-            ) as HTMLElement;
-            notRobotButton?.click();
-            observer.disconnect();
-            console.log("disconnected");
-        }
-    });
-    const target = document.querySelector("body") as HTMLElement;
-    const config = { attributes: true, childList: true, subtree: true };
-    observer.observe(target, config);
+  const observer = new MutationObserver(() => {
+    let notRobotDiv = document.querySelector(
+      ".message.success.loggedin"
+    ) as HTMLElement;
+    if (notRobotDiv !== null) {
+      let notRobotButton = document.querySelector(
+        ".captcha_container"
+      ) as HTMLElement;
+      notRobotButton?.click();
+      observer.disconnect();
+      console.log("disconnected");
+    }
+  });
+  const target = document.querySelector("body") as HTMLElement;
+  const config = { attributes: true, childList: true, subtree: true };
+  observer.observe(target, config);
 
-    var captchaInsert = document.getElementById(
-        "cp-user-input"
+  var captchaInsert = document.getElementById(
+    "cp-user-input"
+  ) as HTMLInputElement;
+
+  const observer2 = new MutationObserver(() => {
+    captchaInsert = document.getElementById(
+      "cp-user-input"
     ) as HTMLInputElement;
 
-    const observer2 = new MutationObserver(() => {
-        captchaInsert = document.getElementById(
-            "cp-user-input"
-        ) as HTMLInputElement;
-
-        if (captchaInsert !== null) {
-            captchaInsert.focus();
-            observer2.disconnect();
-            console.log("disconnected2");
-            captchaSubmit(captchaInsert);
-        }
-    });
-    const target2 = document.querySelector("body") as HTMLElement;
-    const config2 = { attributes: true, childList: true, subtree: true };
-    observer2.observe(target2, config2);
+    if (captchaInsert !== null) {
+      captchaInsert.focus();
+      observer2.disconnect();
+      console.log("disconnected2");
+      captchaSubmit(captchaInsert);
+    }
+  });
+  const target2 = document.querySelector("body") as HTMLElement;
+  const config2 = { attributes: true, childList: true, subtree: true };
+  observer2.observe(target2, config2);
 };
 
 const main = () => {
-    if (document.readyState === "complete") {
-        var isAuthCalled = false;
-        const linkInput = document.querySelector(
-            ".hw-header-input"
-        ) as HTMLInputElement;
-        const searchButton = document.querySelector(
-            ".hw-header-button"
-        ) as HTMLButtonElement;
-        linkInput?.focus();
-        linkInput?.addEventListener("keydown", (e) => {
-            let keyboardEvent = <KeyboardEvent>e;
-            if (keyboardEvent.key === "Enter" && isAuthCalled === false) {
-                isAuthCalled = true;
-                console.log("Enter pressed for search");
-                searchButton?.click();
-                captcha();
-                main();
-            }
-        });
+  if (document.readyState === "complete") {
+    var isAuthCalled = false;
+    const linkInput = document.querySelector(
+      ".hw-header-input"
+    ) as HTMLInputElement;
+    const searchButton = document.querySelector(
+      ".hw-header-button"
+    ) as HTMLButtonElement;
+    linkInput?.focus();
+    linkInput?.addEventListener("keydown", (e) => {
+      let keyboardEvent = <KeyboardEvent>e;
+      if (keyboardEvent.key === "Enter" && isAuthCalled === false) {
+        isAuthCalled = true;
+        console.log("Enter pressed for search");
+        searchButton?.click();
+        captcha();
+        main();
+      }
+    });
 
-        searchButton.onclick = () => {
-            if (isAuthCalled === false) {
-                isAuthCalled = true;
-                captcha();
-                main();
-            }
-        };
-        console.log("in main");
-    } else {
-        console.log("not complete");
-        setTimeout(() => {
-            main();
-        }, 200);
-    }
+    searchButton.onclick = () => {
+      if (isAuthCalled === false) {
+        isAuthCalled = true;
+        captcha();
+        main();
+      }
+    };
+    console.log("in main");
+  } else {
+    console.log("not complete");
+    setTimeout(() => {
+      main();
+    }, 200);
+  }
 };
 
 main();
--- msuQuickLogin/msuQuickLogin.ts
+++ msuQuickLogin/msuQuickLogin.ts
@@ -11,61 +11,61 @@
 // ==/UserScript==
 
 const auth = () => {
-  setTimeout(() => {}, 300);
-  const observer = new MutationObserver(() => {
-    let recieveSmsCode = document.querySelector(
-      "input[value='Receive a code via SMS']"
-    ) as HTMLElement;
-    if (recieveSmsCode !== null) {
-      recieveSmsCode.click();
-      observer.disconnect();
-      console.log("disconnected");
-    }
-  });
+    setTimeout(() => {}, 300);
+    const observer = new MutationObserver(() => {
+        let recieveSmsCode = document.querySelector(
+            "input[value='Receive a code via SMS']"
+        ) as HTMLElement;
+        if (recieveSmsCode !== null) {
+            recieveSmsCode.click();
+            observer.disconnect();
+            console.log("disconnected");
+        }
+    });
 
-  const target = document.querySelector("body") as HTMLElement;
-  const config = { attributes: true, childList: true, subtree: true };
-  observer.observe(target, config);
+    const target = document.querySelector("body") as HTMLElement;
+    const config = { attributes: true, childList: true, subtree: true };
+    observer.observe(target, config);
 };
 const main = () => {
-  var url = window.location.href;
-  setTimeout(() => {
-    if (url.split("/")[2] === "d2l.msu.edu") {
-      url = url.split("/").slice(0, 5).toString();
-    }
-    console.log("in intimeout");
+    var url = window.location.href;
+    setTimeout(() => {
+        if (url.split("/")[2] === "d2l.msu.edu") {
+            url = url.split("/").slice(0, 5).toString();
+        }
+        console.log("in intimeout");
 
-    switch (url) {
-      case "https://s2.lite.msu.edu/adm/roles?source=login":
-        let selectButton = document.getElementsByName(
-          "stmsu7Z2535691a0b16369msul1001"
-        )[0];
-        selectButton.click();
-        break;
-      case "https://loncapa.msu.edu/adm/login":
-        let loginButton = document.getElementById("LC_sso_login_link");
-        let hrefLink = loginButton?.getAttribute("href");
-        window.location.replace(hrefLink!);
-        break;
-      case "https:,,d2l.msu.edu,d2l,loginh":
-        let loginbutton = document.getElementById("loginUrl1");
-        let hreflink = loginbutton?.getAttribute("href");
-        window.location.replace(hreflink!);
-        break;
-      case "https://student.msu.edu/splash.html":
-        let loginbutton1 = document.getElementById("loginUrl1");
-        let hreflink2 = loginbutton1?.getAttribute("href");
-        window.location.replace(hreflink2!);
-        break;
-      default:
-        console.log("outed");
-    }
-  }, 50);
+        switch (url) {
+            case "https://s2.lite.msu.edu/adm/roles?source=login":
+                let selectButton = document.getElementsByName(
+                    "stmsu7Z2535691a0b16369msul1001"
+                )[0];
+                selectButton.click();
+                break;
+            case "https://loncapa.msu.edu/adm/login":
+                let loginButton = document.getElementById("LC_sso_login_link");
+                let hrefLink = loginButton?.getAttribute("href");
+                window.location.replace(hrefLink!);
+                break;
+            case "https:,,d2l.msu.edu,d2l,loginh":
+                let loginbutton = document.getElementById("loginUrl1");
+                let hreflink = loginbutton?.getAttribute("href");
+                window.location.replace(hreflink!);
+                break;
+            case "https://student.msu.edu/splash.html":
+                let loginbutton1 = document.getElementById("loginUrl1");
+                let hreflink2 = loginbutton1?.getAttribute("href");
+                window.location.replace(hreflink2!);
+                break;
+            default:
+                console.log("outed");
+        }
+    }, 50);
 
-  var url = window.location.href;
-  if (url.split("/")[2] === "auth.msu.edu") {
-    auth();
-  }
+    var url = window.location.href;
+    if (url.split("/")[2] === "auth.msu.edu") {
+        auth();
+    }
 };
 main();
 
